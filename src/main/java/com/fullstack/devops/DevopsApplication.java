61f1a4a0944c7e76687ef529e482194da0c36a8e
unknown <ananth.inspire@gmail.com>
2019-12-27 16:09:10 +0100

    Springbootintializer extended

--- src/main/java/com/fullstack/devops/DevopsApplication.java
+++ src/main/java/com/fullstack/devops/DevopsApplication.java
@@ -2,10 +2,16 @@ package com.fullstack.devops;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DevopsApplication {
+public class DevopsApplication extends SpringBootServletInitializer {
 
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DevopsApplication.class);
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsApplication.class, args);
 	}
